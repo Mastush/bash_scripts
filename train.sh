b0a93ff384a5c3d221bea2589921b70d12de1aa6
Mastush <mastush2@gmail.com>
2020-06-24 19:35:39 +0300

    specify python3

--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@
 
 source /cs/labs/amitd/nadavsch/nadav_env/bin/python
 
-python ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 
+python3 ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 
