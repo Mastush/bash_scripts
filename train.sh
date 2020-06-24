f52e8c44b3cd41e097035acefed44ab089e57d39
Mastush <mastush2@gmail.com>
2020-06-24 17:13:25 +0300

    conv attempt

--- train.sh
+++ train.sh
@@ -1,2 +1,5 @@
-#!/nadavsch/bin/bash
-echo success
+#!/bin/csh
+
+source /cs/labs/amitd/nadavsch/nadav_env/bin/python
+
+python ~/Desktop/Representation_Learning/main.py -r 1 -q 7 --network_type conv
