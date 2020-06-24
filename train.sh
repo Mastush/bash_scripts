c2cc2e470cdc87e53d86464d34a10a83d52b407d
Mastush <mastush2@gmail.com>
2020-06-24 19:23:37 +0300

    conv net attempt

--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@
 
 source /cs/labs/amitd/nadavsch/nadav_env/bin/python
 
-python ~/Desktop/Representation_Learning/main.py -r 1 -q 7 --network_type conv
+python ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 
