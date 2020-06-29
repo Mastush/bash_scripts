4af5aaf007b3d81d578d8737a1a349e666f45a78
Mastush <mastush2@gmail.com>
2020-06-29 13:11:12 +0300

    add rounds duh

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@
 
 source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
-python ~/Desktop/Representation_Learning/main.py -q 9 --network_type conv 
+python3 ~/Desktop/Representation_Learning/main.py -r 3 -q 9 --network_type conv 
