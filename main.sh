d83fdecc2f14a1fd36a577eec37691d438ca8fcc
Mastush <mastush2@gmail.com>
2020-06-29 13:48:48 +0300

    add prints for debugging

--- main.sh
+++ main.sh
@@ -1,5 +1,12 @@
 #!/bin/csh
 
+echo "activating virtualenv"
+
 source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
+echo "virtualenv activated"
+echo "running script"
+
 python3 ~/Desktop/Representation_Learning/main.py -r 3 -q 9 --network_type conv 
+
+echo "done!"
