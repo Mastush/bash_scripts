8b4446e06205131603a9d66908c8ae4f30943a4a
Mastush <mastush2@gmail.com>
2020-06-30 21:17:27 +0300

    forgot conv...

--- train.sh
+++ train.sh
@@ -4,4 +4,4 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -q 30 -l 3 --network_type simple -b 64 > ~/nadav_train_output.txt
+python3 -u ~/Desktop/Representation_Learning/network_training.py -q 30 -l 3 --network_type conv -b 64 > ~/nadav_train_output.txt
