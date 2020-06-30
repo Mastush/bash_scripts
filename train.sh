ebc9307d751f9c2b52ac4663af7811acd7fbe32a
Mastush <mastush2@gmail.com>
2020-06-30 22:08:53 +0300

    does want outputs to override each other

--- train.sh
+++ train.sh
@@ -4,12 +4,12 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 3 --network_type simple -b 64 -lr 1 > ~/nadav_train_output.txt
+python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 3 --network_type simple -b 64 -lr 1
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 3 --network_type simple -b 64 -lr 0.1 > ~/nadav_train_output.txt
+python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 3 --network_type simple -b 64 -lr 0.1 
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 3 --network_type simple -b 64 -lr 0.01 > ~/nadav_train_output.txt
+python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 3 --network_type simple -b 64 -lr 0.01
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 3 --network_type simple -b 64 -lr 0.001 > ~/nadav_train_output.txt
+python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 3 --network_type simple -b 64 -lr 0.001 
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 3 --network_type simple -b 64 -lr 0.0001 > ~/nadav_train_output.txt
+python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 3 --network_type simple -b 64 -lr 0.0001
