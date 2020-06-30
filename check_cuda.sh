e172aadb129c2bd301c504732e66b29d23b048b3
Mastush <mastush2@gmail.com>
2020-06-30 10:14:09 +0300

    whatever

--- check_cuda.sh
+++ check_cuda.sh
@@ -3,4 +3,4 @@
 
 source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
-python3 -u ~/Desktop/Representation_Learning/main.py > ~/nadav_output.txt
+python3 -u ~/Desktop/Representation_Learning/gpu_tests.py > ~/nadav_output.txt
