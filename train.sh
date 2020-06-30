4eccad4663c03fe29fc3c449475515bc2f4664fe
Mastush <mastush2@gmail.com>
2020-06-30 21:43:01 +0300

    switch to simple

--- train.sh
+++ train.sh
@@ -4,4 +4,4 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -q 30 -l 3 --network_type conv -b 64 -lr 0.00001 > ~/nadav_train_output.txt
+python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 3 --network_type simple -b 64 -lr 0.00001 > ~/nadav_train_output.txt
