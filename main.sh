520892d0a41c20e4fc4029ef6c8a0dc4d1c38953
Mastush <mastush2@gmail.com>
2020-06-30 15:21:17 +0300

    ...

--- main.sh
+++ main.sh
@@ -1,12 +1,6 @@
 #!/bin/csh
 
-echo "activating virtualenv"
 
 source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
-echo "virtualenv activated"
-echo "running script"
-
-python3 ~/Desktop/Representation_Learning/main.py -r 3 -q 9 --network_type conv 
-
-echo "done!"
+python3 ~/Desktop/Representation_Learning/main.py -r 3 -q 9 --network_type conv > ~/nadav_output.txt
--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@
 
 source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
-python ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 
+python ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 > ~/nadav_train_output.txt
