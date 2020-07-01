693a9fe7fa618d23c05d61a5c18a3d626218d6a9
Mastush <mastush2@gmail.com>
2020-07-01 11:12:52 +0300

    try n=100 and check cuda usage

--- main.sh
+++ main.sh
@@ -5,4 +5,4 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u ~/Desktop/Representation_Learning/main.py -r 3 -q 9 --network_type conv > ~/nadav_output.txt
+python3 -u ~/Desktop/Representation_Learning/main.py -r 3 -q 9 --network_type conv --n_train 100 --n_test 100 > ~/nadav_output.txt
