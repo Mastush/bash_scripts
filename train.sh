55e6222a8d692ffdaee736026a4abbef6dd6af27
Mastush <mastush2@gmail.com>
2020-07-01 12:23:12 +0300

    now try with dim red

--- train.sh
+++ train.sh
@@ -4,4 +4,4 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -q 50 -l 5 --network_type simple -b 64 --normalize_raw
+python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 --normalize_raw
