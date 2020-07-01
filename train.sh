73b2896b3c7cef23f4b041887508b6df436efedb
Mastush <mastush2@gmail.com>
2020-07-01 11:31:39 +0300

    try no dim red, also normalize raw

--- train.sh
+++ train.sh
@@ -4,12 +4,4 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 1 -o sgd
-
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 0.1 -o sgd
-
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 0.01 -o sgd
-
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 0.001 -o sgd
-
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 0.0001 -o sgd
+python3 -u ~/Desktop/Representation_Learning/network_training.py -q 50 -l 5 --network_type simple -b 64 --normalize_raw
