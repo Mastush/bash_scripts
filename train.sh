ff8f3a97afef2d24bd12032ca33804620ea61112
Mastush <mastush2@gmail.com>
2020-07-01 11:00:57 +0300

    try sgd

--- train.sh
+++ train.sh
@@ -4,12 +4,12 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 1
+python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 1 -o sgd
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 0.1 
+python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 0.1 -o sgd
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 0.01
+python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 0.01 -o sgd
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 0.001 
+python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 0.001 -o sgd
 
-python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 0.0001
+python3 -u ~/Desktop/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 -lr 0.0001 -o sgd
