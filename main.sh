41f02c85772f65e4571f834ae98cdd2b986fcf5e
Mastush <mastush2@gmail.com>
2020-07-02 15:21:50 +0300

    change to cifar...

--- main.sh
+++ main.sh
@@ -5,6 +5,6 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u ~/Desktop/Representation_Learning/main.py -r 1 -d 50 -q 49 --network_type simple
+python3 -u ~/Desktop/Representation_Learning/main.py -r 1 -d 50 -q 49 --network_type simple --dataset cifar
 
-python3 -u ~/Desktop/Representation_Learning/main.py -r 2 -d 50 -q 49 --network_type simple
+python3 -u ~/Desktop/Representation_Learning/main.py -r 2 -d 50 -q 49 --network_type simple --dataset cifar
