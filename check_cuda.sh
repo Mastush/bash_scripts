abd7e31e469f56ec6b962ad4a0c3f7b514edefe6
Mastush <mastush2@gmail.com>
2020-07-05 12:24:57 +0300

    update paths

--- check_cuda.sh
+++ check_cuda.sh
@@ -6,4 +6,4 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 module avail cuda
 module load cuda
 
-python3 -u ~/Desktop/Representation_Learning/gpu_tests.py > ~/nadav_cuda_output.txt
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/gpu_tests.py > ~/nadav_cuda_output.txt
