79551070ea4ab50f9706ceb92d9cf7ff98a30855
Mastush <mastush2@gmail.com>
2020-07-05 16:49:44 +0300

    conv cifar training

--- train.sh
+++ train.sh
@@ -4,4 +4,4 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple -b 64 --normalize_raw
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/network_training.py -q 30 -l 5 --network_type conv --dataset cifar
