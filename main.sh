ca27bb55f9f09fc45c72e579a7b87eb2dc01f6ee
Mastush <mastush2@gmail.com>
2020-07-06 15:26:01 +0300

    5000 iter maybe?

--- main.sh
+++ main.sh
@@ -5,8 +5,8 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -d 75 -q 74 --network_type simple --dataset cifar -a 0.0001 -ae 0.0001
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -d 75 -q 74 --network_type simple --dataset cifar -a 0.0001 -ae 0.0001 -m 5000 -me 5000
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 2 -d 75 -q 74 --network_type simple --dataset cifar -a 0.0001 -ae 0.0001
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 2 -d 75 -q 74 --network_type simple --dataset cifar -a 0.0001 -ae 0.0001 -m 5000 -me 5000
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 3 -d 75 -q 74 --network_type simple --dataset cifar -a 0.0001 -ae 0.0001
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 3 -d 75 -q 74 --network_type simple --dataset cifar -a 0.0001 -ae 0.0001 -m 5000 -me 5000
