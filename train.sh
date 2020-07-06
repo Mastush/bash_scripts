671df74802f9b2e702920081e4f03a5a1d76104d
Mastush <mastush2@gmail.com>
2020-07-06 12:04:54 +0300

    another round of nets for cifar

--- train.sh
+++ train.sh
@@ -4,6 +4,6 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/network_training.py -q 30 -l 3 --network_type conv --dataset cifar
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/network_training.py -q 30 -l 3 --network_type conv --dataset cifar -lr 0.001
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/network_training.py -d 50 -q 50 -l 5 --network_type simple --dataset cifar -lr 0.001
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/network_training.py -q 50 -l 5 --network_type simple --dataset cifar -lr 0.001
