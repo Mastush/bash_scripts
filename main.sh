f0175e8a737f4d96a486bdbbadaa15b425b5668a
Mastush <mastush2@gmail.com>
2020-07-06 10:17:58 +0300

    try conv mnist again

--- main.sh
+++ main.sh
@@ -5,6 +5,4 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -d 50 -q 49 --network_type simple --dataset cifar
-
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 2 -d 50 -q 49 --network_type simple --dataset cifar
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 3 -q 9 --network_type conv
