c9e0c8716f7e52372ce09b56fe4f8ff901687c50
Mastush <mastush2@gmail.com>
2020-07-07 12:40:37 +0300

    try less channels

--- main.sh
+++ main.sh
@@ -5,4 +5,7 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -q 9 --network_type conv --dataset cifar -m 5000 -me 5000
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -q 3 --network_type conv --dataset cifar -m 5000 -me 5000
+
+
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 2 -q 3 --network_type conv --dataset cifar -m 5000 -me 5000
