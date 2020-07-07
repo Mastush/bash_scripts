b41fa8f73613c0caf4cefdcc2aa8d85bdda9e181
Mastush <mastush2@gmail.com>
2020-07-07 12:38:07 +0300

    redo conv for cifar

--- main.sh
+++ main.sh
@@ -5,6 +5,4 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -d 75 -q 74 --network_type simple --dataset cifar -a 0.00001 -ae 0.00001 -m 5000 -me 5000
-
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -d 75 -q 74 --network_type simple --dataset cifar -a 0.0000001 -ae 0.0000001 -m 5000 -me 5000
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -q 9 --network_type conv --dataset cifar -m 5000 -me 5000
