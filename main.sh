2c22c9284d30bbdd3ef26202150f3860f1f21d80
Mastush <mastush2@gmail.com>
2020-07-07 12:20:00 +0300

    some more attempts at alpha values

--- main.sh
+++ main.sh
@@ -5,8 +5,6 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -d 75 -q 74 --network_type simple --dataset cifar -a 0.0001 -ae 0.0001 -m 5000 -me 5000
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -d 75 -q 74 --network_type simple --dataset cifar 0.00001 -ae 0.00001 -m 5000 -me 5000
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 2 -d 75 -q 74 --network_type simple --dataset cifar -a 0.0001 -ae 0.0001 -m 5000 -me 5000
-
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 3 -d 75 -q 74 --network_type simple --dataset cifar -a 0.0001 -ae 0.0001 -m 5000 -me 5000
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -d 75 -q 74 --network_type simple --dataset cifar 0.0000001 -ae 0.0000001 -m 5000 -me 5000
