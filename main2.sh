bc4fe5329a5ade97b5f521340bcb1592b15af274
Mastush <mastush2@gmail.com>
2020-07-08 09:24:34 +0300

    multiple alphas for conv for cifar

---
+++ main2.sh
@@ -0,0 +1,8 @@
+#!/bin/csh
+
+
+source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
+
+module load cuda
+
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -q 9 --network_type conv --dataset cifar -m 5000 -me 5000 -a 0.0001 -ae 0.0001
