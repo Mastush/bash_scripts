e5bc39ef700d09609507d78bbb32d19e7e2b5c24
Mastush <mastush2@gmail.com>
2020-07-08 10:26:10 +0300

    a bit more

--- train.sh
+++ train.sh
@@ -4,6 +4,6 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/network_training.py -q 50 -l 5 --network_type conv --dataset cifar -w 0.0001
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/network_training.py -q 50 -l 5 --network_type conv --dataset cifar -w 0.01
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/network_training.py -q 50 -l 5 --network_type conv --dataset cifar -w 0.001
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/network_training.py -q 50 -l 5 --network_type conv --dataset cifar -w 0.1
