5c13c4046c7cee2b0c5da553456a9e89219b8be0
Mastush <mastush2@gmail.com>
2020-07-14 17:05:00 +0300

    some more

--- main.sh
+++ main.sh
@@ -5,4 +5,4 @@ source /cs/labs/amitd/nadavsch/nadav_env/bin/activate.csh
 
 module load cuda
 
-python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -q 15 --network_type conv --dataset cifar -m 5000 -me 5000 -a 0.0000001 -ae 0.0000001
+python3 -u /cs/labs/amitd/nadavsch/code/Representation_Learning/main.py -r 1 -q 15 --network_type conv --dataset cifar -m 5000 -me 5000 -a 0.0000001 -ae 0.0001
